5afd30ac6ca49e87d2aa035909ec4f4a7ef60944
MDBarbier <matthew-7.7@hotmail.com>
2017-09-17 14:35:51 +0100

    added knockoutJSExample
    simple knockout.js example in an MVC app. Shows UI elements bound to Knockout VM, and the VM communicating with WebAPI controller via AJAX to update observables (which then updated on the GUI automatically).

---
+++ knockoutJsTest/knockoutJsTest/Controllers/APIController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using knockoutJsTest.Models;
+
+namespace knockoutJsTest.Controllers
+{
+    public class APIController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<GamesTable> Get()
+        {
+            return testDataDAL.GetGames();
+        }
+
+        // GET api/<controller>/5
+        public GamesTable Get(int id)
+        {
+            return testDataDAL.GetGames().FirstOrDefault(g => g.Id == id);
+        }
+
+        // POST api/<controller>
+        public HttpResponseMessage Post(GamesTable game)
+        {
+            testDataDAL.InsertGame(game);
+            var response = Request.CreateResponse(HttpStatusCode.Created, game);
+            var url = Url.Link("DefaultApi", new { game.Id });
+            response.Headers.Location = new Uri(url);
+            return response;
+        }
+               
+        // DELETE api/<controller>/5
+        public HttpResponseMessage Delete(int id)
+        {
+            testDataDAL.DeleteGame(id);
+            var response = Request.CreateResponse(HttpStatusCode.OK, id);
+            return response;
+        }
+    }
+}
---
+++ knockoutJsTest/knockoutJsTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="knockoutJsTest.MvcApplication" Language="C#" %>
