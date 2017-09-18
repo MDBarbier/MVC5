10d075329cf1be2b10fc2fc60610a41088b961f6
MDBarbier <matthew-7.7@hotmail.com>
2017-09-18 10:19:35 +0100

    moved vm initialisation ino vm file

---
+++ knockoutJsTest/knockoutJsExample/Controllers/AjaxController.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace knockoutJsExample.Controllers
+{
+    public class AjaxController : ApiController
+    {
+        // GET: api/API
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "test successful" };
+        }
+        
+        // POST: api/API
+        public string Post(JObject value)
+        {
+            string rv = "Hello " + value["lastName"].ToString();
+            return rv;
+        }
+
+        
+    }
+}
---
+++ knockoutJsTest/knockoutJsExample/Scripts/knockoutVM.js
@@ -0,0 +1,35 @@
+﻿
+//storing the vm as a variable could also just have "function ViewModel(){...}" then just new it up when needed
+var ViewModel = function (first, last) {
+
+    var vm = this;
+
+    this.firstName = ko.observable(first);
+    this.lastName = ko.observable(last);
+    this.serverMessage = ko.observable();
+
+    this.fullName = ko.computed(function ()
+    {
+        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.        
+        return this.firstName() + " " + this.lastName();
+    }, this);
+
+    this.postData = function () {
+        var dataObject = ko.toJSON(this);        
+        $.ajax({
+            url: '/api/Ajax',
+            type: 'post',
+            data: dataObject,
+            contentType: 'application/json',
+            success: function (data) {
+
+                window.alert("server says: " + data);
+                vm.serverMessage(data);
+            }
+        });
+    };
+};
+
+var vm = new ViewModel("Planet", "Earth");
+ko.applyBindings(vm); // This makes Knockout get to work
+
