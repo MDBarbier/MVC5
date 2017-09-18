8a42b7db4d7d58b8c83f569fa1e80061c75440c3
MDBarbier <matthew-7.7@hotmail.com>
2017-09-18 16:54:31 +0100

    x
    x

---
+++ knockoutJsMappingTest/mappingTestApp/mappingTestApp/Controllers/AjaxController.cs
@@ -0,0 +1,38 @@
+﻿using mappingTestApp.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace mappingTestApp.Controllers
+{
+    public class AjaxController : ApiController
+    {
+        // GET: api/Ajax
+        public IEnumerable<Game> Get()
+        {
+            return GamesDAL.GetGames();
+        }
+
+        // POST: api/Ajax
+        public HttpResponseMessage Post(Game game)
+        {
+            GamesDAL.InsertGame(game);
+            var response = Request.CreateResponse(HttpStatusCode.Created, game);
+            var url = Url.Link("DefaultApi", new { game.Id });
+            response.Headers.Location = new Uri(url);
+            return response;
+        }
+                
+        // DELETE: api/Ajax/5
+        public HttpResponseMessage Delete(int id)
+        {
+            GamesDAL.DeleteGame(id);
+            var response = Request.CreateResponse(HttpStatusCode.OK, id);
+            return response;
+        }
+    }
+}
---
+++ knockoutJsMappingTest/mappingTestApp/mappingTestApp/Scripts/GameVM.js
@@ -0,0 +1,6 @@
+﻿function GameViewModel() {
+    this.gameName = ko.observable("game name");
+};
+
+//Activate!
+ko.applyBindings(new GameViewModel());
